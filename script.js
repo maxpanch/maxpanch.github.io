95c6c50218dfda74ce7967d1f1d4ee2fd5484268
FireOKayBit <mfire.panchenko@gmail.com>
2023-03-16 19:57:27 +0500

    Header Fixes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function moveToReels(str) {
     const y = document.getElementById(str).getBoundingClientRect().top + window.scrollY;
     yy = y;
     if (str = 'demo_reel') {
-        yy = y - 100;
+        yy = y - 125;
     }
     window.scroll({
     top: yy,
